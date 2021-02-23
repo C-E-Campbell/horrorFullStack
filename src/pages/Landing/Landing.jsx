31c7463b7c036909748afa5ba1e51e3ca98f1025
charles <codelyfewithcharlie@gmail.com>
2021-02-23 15:41:30 -0500

    updates to landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -16,6 +16,30 @@ export default function Landing() {
           <Link to="/signin">THE RULES</Link>
         </div>
       </section>
+
+      <section className={styles.rules_section}>
+        <h2>The Rules</h2>
+        <div className={styles.rules_InnerContainer}>
+          <div className={styles.rules_textContainer}>
+            <p>
+              Watch 100 horror movies in 100 days...<br></br>or a year, you
+              choose!
+            </p>
+            <div className={styles.rules_buttonsContainer}>
+              <div>Easy</div>
+              <div>Murder Me!</div>
+            </div>
+          </div>
+          <div className={styles.rules_moviesContainer}>
+            <div>movie 1</div>
+            <div>movie 2</div>
+            <div>movie 3</div>
+            <div>movie 4</div>
+          </div>
+        </div>
+      </section>
+
+      <footer></footer>
     </div>
   );
 }
